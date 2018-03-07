4fcfe51181342e2aa84325e3ee5d0f9303c21bd3
banbanxia <867645937@qq.com>
2018-03-07 11:52:56 +0800

    ???

--- src/main/java/com/zly/ovo/OvoApplication.java
+++ src/main/java/com/zly/ovo/OvoApplication.java
@@ -5,7 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OvoApplication {
-
+	/**
+	 *作者：张琳玉
+	 *功能描述：测试springBoot
+	 *日期：2018/3/7 11:52
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(OvoApplication.class, args);
 	}
