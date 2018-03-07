0d1b6b06fb4a817e9944802403369d2da92e0f60
banbanxia <867645937@qq.com>
2018-03-07 11:50:41 +0800

    ???

---
+++ src/main/java/com/zly/ovo/OvoApplication.java
@@ -0,0 +1,12 @@
+package com.zly.ovo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OvoApplication.class, args);
+	}
+}
